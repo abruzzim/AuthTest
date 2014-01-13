7b82654caa9f958d795ff07bc60ec5b8b9af2c82
Mario Abruzzi <abruzzim@framework-solutions.com>
2014-01-13 12:43:32 -0500

    AuthTest

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Authtest::Application.config.secret_token = '0d68dabc356e57d18b10e64045488b0f7a72d4cba831021ba51144755d2d9127e7cb0e3706fe939a0dbde41913ccfdb5632bf1fd73a2e46bcb3979ae7a72cec3'
